7f539a87c9b0e453af2e0e024078c09b9be380c1
congruih <qaz51000@163.com>
2020-02-20 16:59:39 +0800

    第三章 项目准备

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <span class="text">ASDFHJKL</span>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { mapGetters } from 'vuex'
+const getters = {
+  a: () => 1,
+  b: () => 2,
+  c: () => 3
+}
+function fn (keys) {
+  const data = {}
+  keys.forEach(key => {
+    if (Object.prototype.hasOwnProperty.call(getters, key)) {
+      data[key] = getters[key]
+    }
+  })
+  return data
 }
 
-#nav {
-  padding: 30px;
+export default {
+  computed: {
+    ...mapGetters(['test']),
+    ...fn(['a', 'b', 'c'])
+  },
+  mounted () {
+    this.$store.dispatch('setTest', 9).then(() => {
+      console.log(this.test)
+    })
+    console.log(this.a, this.b, this.c)
+  }
 }
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  @import "./assets/styles/global";
+  .text {
+    font-family: "Days One";
+    font-size: px2rem(20);
+    color: orange;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="icon-bookmark"></span>
+    <span class="icon-clock"></span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+<style scoped>
+  .icon-bookmark {
+    font-size: 30px;
+    color: red;
+  }
+</style>
